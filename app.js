d3101222dc7d84d704fbc534108c49fe3d4c0d51
Ferxxo <garcilun02@gmail.com>
2025-07-26 20:14:25 -0600

    Se agregaron las siguientes funciones: mostrarAmigos(), sortearAmigo(), agregarAmigo() y limpiarAmigo(). Concluyendo el challenge.

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const amigos = [];
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    if (!amigo){
+        alert('Por favor, ingresa un nombre de amigo.');
+        return;
+    }
+    amigos.push(amigo);
+    mostrarAmigos();
+    limpiarInput();
+    return;
+}
+
+function mostrarAmigos(){
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ''; 
+    for (let i = 0; i < amigos.length; i++) {
+        document.getElementById('listaAmigos').innerHTML += `<li>${amigos[i]}</li>`;
+    }
+    return;
+}
+
+function sortearAmigo(){
+    if(!amigos.length){
+        alert('No hay amigos para sortear.');
+        return;
+    }else{
+        const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        const amigoSorteado = amigos[indiceAleatorio];
+        document.getElementById('resultado').innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+    }
+    return;
+}
+
+function limpiarInput(){ 
+    document.getElementById('amigo').value = '';
+    document.getElementById('amigo').focus();
+    return;
+}
